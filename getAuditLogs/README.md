1329c317a1b06faed516c4f3c30f2875c323b3c0
chandrashekar-cohesity <chandrashekar.dashudu@cohesity.com>
2020-08-26 23:10:44 -0700

    read input from command line

--- getAuditLogs/README.md
+++ getAuditLogs/README.md
@@ -6,15 +6,28 @@
 
 ### Run script
 
-* Run the script using
+* Script help
 
   ```
-    python getAuditLogs.py
+  python getAuditLogs.py -h
+  ```
+
+* Run the script using with all options
+
+  ```
+    python getAuditLogs.py -t 10.2.171.30 -u admin -p admin -s 08/01/2020 -e 08/03/2020 -c logs.csv
+  ```
+
+* Run the script using default options
+  
+  ```
+    # See the default values using `python getAuditLogs.py -h`
+    python getAuditLogs.py 
   ```
 
 * The script will prompt you some questions regarding Cohesity Cluster and other information. 
 
-* Enter the correct values and the script will output a csv file (default is Auditlogs.csv)
+* Enter the correct values and the script will output a csv file (default is output.csv)
 
 ### Have any question
 
--- getAuditLogs/getAuditLogs.py
+++ getAuditLogs/getAuditLogs.py
@@ -1,4 +1,4 @@
-
+import argparse
 import requests
 import urllib3
 import time
@@ -6,9 +6,21 @@ import json
 import csv
 import math
 import datetime
+import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 requests.packages.urllib3.add_stderr_logger()
+
+def valid_date(s):
+    """
+    Validate the function to be in the correct date-time format
+    """
+    try:
+        return datetime.datetime.strptime(s, '%m/%d/%Y').date().strftime('%m/%d/%Y')
+        #return datetime.datetime.date().strftime('%m/%d/%Y')
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
 
 def request(method, path=None, data=None, params=None, uri=None, auth=None, content_type="application/json", accept="application/json"):
     headers = {}
@@ -30,7 +42,7 @@ def request(method, path=None, data=None, params=None, uri=None, auth=None, cont
     
     r = requests.request(method, uri, headers=headers,params=params,
                          data=payload, verify=False)
-
+    
     if 200 <= r.status_code <= 299:
         return r
 
@@ -82,28 +94,45 @@ def get_logs(start_index):
         temp_log[ "newRecord"] = logs.get("newRecord")
         master_list.append(temp_log.copy())
 
-def convert_to_csv(dict_array, job_name):
-    
+def convert_to_csv(dict_array, output_file_name):
     keys = ["timeStamp", "userName", "domain", "action", "entityName", "entityType", "entityId", "details", "ip","clusterInfo", "previousRecord", "newRecord" ]
-    with open(job_name + '.csv', 'a') as output_file:
+    with open(output_file_name, 'a') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(dict_array)
-    print("Wrote a file "+ job_name + ".csv in current directory")
+    print("Wrote a file "+ output_file_name + " in current directory")
 
 
 token = None
 
-cluster_endpoint = raw_input("Enter your cluster FQDN or IP : ")
-username = raw_input("Enter your cluster username(admin) : ") or "admin"
-password = raw_input("Enter your cluster password(admin) : ") or "admin"
-org = raw_input("Enter your cluster org name(LOCAL) : ") or "LOCAL"
-filename = raw_input("Enter the CSV file name(AuditLogs.csv) : ") or "AuditLogs"
+# cluster_endpoint = raw_input("Enter your cluster FQDN or IP : ")
+# username = raw_input("Enter your cluster username(admin) : ") or "admin"
+# password = raw_input("Enter your cluster password(admin) : ") or "admin"
+# org = raw_input("Enter your cluster org name(LOCAL) : ") or "LOCAL"
+# filename = raw_input("Enter the CSV file name(AuditLogs.csv) : ") or "AuditLogs"
+
+date_now = datetime.datetime.now().date().strftime('%m/%d/%Y')
+thirty_days_before_now = (datetime.datetime.now() - datetime.timedelta(30)).date().strftime('%m/%d/%Y')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--target', default='localhost', help='IP address of the target machine in cluster. Defaults to localhost')
+parser.add_argument('-u', '--username', default='admin', help='Username. Defaults to admin')
+parser.add_argument('-p', '--password', default='admin', help='Password. Defaults to admin')
+parser.add_argument('-c', '--csvfile', default='output.csv', help='CSV output filename. Defaults to output.csv)')
+parser.add_argument('-s', '--startdate',default=thirty_days_before_now,help='Start date for runs. Format mm/dd/yyyy. Defaults to 30 before days from today', type=valid_date)
+parser.add_argument('-e', '--enddate', default=date_now, help='End date for logs(has to be after startdate if provided). Format mm/dd/yyyy. Defaults to today', type=valid_date)
+
+args = parser.parse_args()
+cluster_endpoint = str(args.target)
+username = str(args.username)
+password = str(args.password)
+filename = str(args.csvfile)
+start_date_input = str(args.startdate)
+end_date_input = str(args.enddate)
 
 date_now = datetime.datetime.now().date().strftime('%m/%d/%Y')
 date_now_epoch = time.mktime(datetime.datetime.now().timetuple())*1000000.0
-start_date_input = raw_input("Enter start date to generate report(mm/dd/yyyy)") 
-end_date_input = raw_input("Enter end date to generate report (mm/dd/yyyy)")
 
 print("###NOTE: The results ")
 epoch_start_date_input = '{:f}'.format((((datetime.datetime(int(start_date_input.split('/')[2]),int(start_date_input.split('/')[0]),int(start_date_input.split('/')[1])) - datetime.datetime(1970,1,1)).total_seconds())*1000000.0))
